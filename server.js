a93001c72bf30531650ca275063a90fe71b438a8
João Pedro Alves de Souza <jsouza.1650@gmail.com>
2021-05-12 18:11:59 -0300

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log(`User connected with id: ${socket.id}`)
+
+    socket.on('new message', (message) => {
+        console.log('User message is: ' + message)
+    })
+
+})
+
+server.listen(3000, () => {
+    console.log('Server on in http://localhost: 3000')
+})
